b7a3c7c2853de13cee7cbbd0c1071d3be117145a
EL GATO <142356190+Scriptbros@users.noreply.github.com>
2023-09-15 07:30:29 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
 
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html'); // Create this HTML file later
+});
+
+io.on('connection', (socket) => {
+  console.log('a user connected');
+  socket.on('chat message', (msg) => {
+    io.emit('chat message', msg);
+  });
+  socket.on('disconnect', () => {
+    console.log('user disconnected');
+  });
+});
+
+server.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+    
